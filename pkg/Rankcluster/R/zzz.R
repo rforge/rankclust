8c5ab57f702d00a03cdd3f495d030d6daf4c2444
grimonprez <grimonprez@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-04 17:28:07 +0000

    update zzz.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rankclust@38 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Rankcluster/R/zzz.R
+++ pkg/Rankcluster/R/zzz.R
@@ -1,5 +1,5 @@
 #' @export
-.onLoad <- function(libname = find.package("Rankcluster"), pkgname = "Rankcluster") 
+.onAttach <- function(lib,pkg)
 {
   packageStartupMessage("WARNING : Since Rancluster 0.92, the ranks have to be given to the package in the ranking notation (see convertRank function), with the following convention :
 - missing positions are replaced by 0
